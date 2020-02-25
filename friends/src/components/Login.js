4ec1fa594b4ac5e9470e02ddd7e2250ef2a5d48d
Alicia Petgrave <alicia.petgrave@gmail.com>
2020-02-25 16:38:26 -0500

    adding axioswithauth, private route, and setting up state for login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,26 +1,53 @@
 import React, {useState} from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth'
 
-function Login() {
+function Login(props) {
 
     const [login, setLogin] = useState({
         username: '',
         password: ''
+    })
+
+    const handleChanges = e => {
+          setLogin({
+              ...login,
+              [e.target.username]: e.target.value
       })
+    }  
 
-
+    const submitLogin = e => {
+        e.preventDefault()
+        axiosWithAuth()
+        .post('/api/login', login)
+        .then(response => {
+            window.localStorage.setItem('token', response.data.payload)
+            // history.push('/protected')
+        })
+        .catch(error => {
+            console.log (error)
+        })
+        setLogin({
+            username:'',
+            password:''
+        })
+    }
 
     return (
         <div>
-            <form>
+            <form onSubmit={submitLogin}>
                 <input
                 type="text"
                 placeholder="username"
-                name="username"/>
+                name="username"
+                onChange={handleChanges}
+                value={login.username}/>
 
                 <input
                 type="text"
                 placeholder="password"
-                name="password"/>
+                name="password"
+                onChange={handleChanges}
+                value={login.password}/>
             </form>
             <button type="submit">Login</button>
         </div>
